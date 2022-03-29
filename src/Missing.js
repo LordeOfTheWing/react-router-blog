f3ab55dc853859e094f5c28bb2b93c9193d2f14e
Clinton Mainya <klyntonmainya@gmail.com>
2022-03-29 17:19:01 +0300

    Structure the missing page component

--- src/Missing.js
+++ src/Missing.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Missing = () => {
   return (
-    <main>
-      <h1>Missing</h1>
+    <main className='Missing'>
+      <h2>Oops... Page Not Found</h2>
+      <p>Well, that's embarrassing.</p>
+      <p>
+        <Link to='/'>Visit Our HomePage</Link>
+      </p>
     </main>
   );
 };
